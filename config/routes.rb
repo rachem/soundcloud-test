eceeff86962a9e6bd76225260cf7902757c276f8
rachem <rachem.goruchon@ingesup.com>
2013-12-04 09:44:07 +0100

    Add Sign out !

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 SoundcloudTest::Application.routes.draw do
 
+    #root to: "home/index"
     root to: "users#new"
     get "/auth/:provider/callback" => "sessions#create"
     get "/signout" => "sessions#destroy", :as => :signout
